1876409590625ff10e809a5d759c543e34ff0ba8
Yuan Fu <yuanfu_bio@163.com>
2025-03-01 16:00:48 +0800

    update summary functions

--- scripts/summary.py
+++ scripts/summary.py
@@ -17,7 +17,8 @@ def setup_and_parse_args():
 def parse_json_config(config):
     barcode_struct = config['barcode_struct']
     barcode1, barcode2 = barcode_struct["barcode1"], barcode_struct["barcode2"]
-    return barcode1, barcode2
+    FB_info_file = config["feature_barcode_info"]
+    return barcode1, barcode2, FB_info_file
 
 def dict2df(dict):
     df = pd.DataFrame.from_dict(dict, orient='index')
@@ -28,17 +29,23 @@ def dict2df(dict):
 if __name__ == "__main__":
     args = setup_and_parse_args()
     config = read_json_config(args.config)
-    barcode1, barcode2 = parse_json_config(config)
+    barcode1, barcode2, FB_info_file = parse_json_config(config)
     barcodes = barcode1 + barcode2
     summary_dir = os.path.join(args.output, "00_summary")
-    os.makedirs(summary_dir, exist_ok=True)
+    
+    if FB_info_file == "":
+        print("feature_barcode_info is not specified, continue.")
+    else:
+        FB_info = pd.read_csv(FB_info_file, sep = "\t", header = None)
+        FB_info.columns = ["Code", "FB", "Info"]
+        FB_dict = FB_info.set_index('Code')['Info'].to_dict()
 
     validation_dict = {}
-    df_counts_dict = {}
-    df_saturation_dict = {}
+    counts_dict = {}
     for sample in args.samples.split():
         log_dir = os.path.join(args.output, sample, "00_logs")
-        count_dir = os.path.join(args.output, sample, "03_counts")
+        saturation_dir = os.path.join(args.output, sample, "04_saturation")
+
         # 汇总barcode有效率
         validation_dict[sample] = {}
         for barcode in barcodes:
@@ -47,23 +54,20 @@ if __name__ == "__main__":
                 barcode_dict = json.load(f)
                 validation_dict[sample][barcode] = barcode_dict['barcode_valid_percent']
                 total_reads = int(barcode_dict['total_reads'])
-        # 汇总counts与Saturation
-        counts_saturation_file = os.path.join(count_dir, f"{sample}_Saturation.tsv")
-        if os.path.exists(counts_saturation_file):
-            df_counts_saturation = pd.read_csv(counts_saturation_file, sep = "\t", index_col=0)
-            df_counts_dict[sample] = df_counts_saturation["UMI Counts"]
-            df_saturation_dict[sample] = df_counts_saturation["Saturation"]
-        else:
-            print(f"{sample}_Saturation.tsv dosn't exist.")
+
+        # 汇总counts
+        final_map = os.path.join(saturation_dir, f"{sample}_per_bc_umi_count_after_downsample.map")
+        df_map = pd.read_csv(final_map, sep="\t", header=None)
+        df_map.columns = ["Barcode1", "Code", "Counts"]
+        df_summary = df_map.groupby("Code")["Counts"].sum().reset_index()
+        df_counts = df_summary.merge(FB_info, on="Code", how="right")
+        df_counts.set_index("Info", inplace=True)
+        counts_dict[sample] = df_counts["Counts"]
 
     df_validation = dict2df(validation_dict)
-    if os.path.exists(counts_saturation_file):
-        df_counts = dict2df(df_counts_dict)
-        df_saturation = dict2df(df_saturation_dict)
+    df_counts = dict2df(counts_dict)
 
     file_path = os.path.join(summary_dir, f"summary.xlsx")
     with pd.ExcelWriter(file_path, engine='xlsxwriter') as writer:
         df_validation.to_excel(writer, sheet_name="Validation")
-        if os.path.exists(counts_saturation_file):
-            df_counts.to_excel(writer, sheet_name="Counts")
-            df_saturation.to_excel(writer, sheet_name="Saturation")
+        df_counts.to_excel(writer, sheet_name="Counts")
